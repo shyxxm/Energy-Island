ebb178fe13aaaf4e31e632e7d48ccf591d3012dd
junwei9955 <junwei10255@gmail.com>
2023-03-08 14:48:40 +0800

    static file location in settings.py

--- islandproject_root/islandproject/settings.py
+++ islandproject_root/islandproject/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(CORE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+    BASE_DIR / "apps/static",
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
